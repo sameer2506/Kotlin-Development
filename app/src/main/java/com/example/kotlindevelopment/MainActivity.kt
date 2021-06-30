b510a27e473753c82bd3bd2682840141d24d6daf
sameer2506 <61904042+sameer2506@users.noreply.github.com>
2021-06-30 12:57:52 +0530

    Age calculating app

--- app/src/main/java/com/example/kotlindevelopment/MainActivity.kt
+++ app/src/main/java/com/example/kotlindevelopment/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.kotlindevelopment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buClickEvent(view: View) {
+
+        val userDOB: String = etDOB.text.toString()
+        if (userDOB.toInt() == 0) {
+            tvShowAge.text = "Invalid input."
+            return
+        }
+        val year: Int = Calendar.getInstance().get(Calendar.YEAR)
+        val myAge = year - userDOB.toInt()
+        val avg = myAge / userDOB.toInt()
+        tvShowAge.text = "Your age is $myAge"
     }
 }
