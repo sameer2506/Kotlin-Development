57e01ba1098837940b6f85a829e33044fa2be9f6
sameer2506 <61904042+sameer2506@users.noreply.github.com>
2021-07-04 19:43:07 +0530

    pass data between activity of listitem

---
+++ app/src/main/java/com/example/kotlindevelopment/Food.kt
@@ -0,0 +1,12 @@
+package com.example.kotlindevelopment
+
+class Food {
+    var name:String? =null
+    var des:String? =null
+    var image:Int? =null
+    constructor(name:String,des:String,image:Int){
+        this.name=name
+        this.des=des
+        this.image=image
+    }
+}
