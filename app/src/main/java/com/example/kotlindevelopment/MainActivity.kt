d036c700068cf5fb039236e28191e9dddfcc2fcf
sameer2506 <61904042+sameer2506@users.noreply.github.com>
2021-07-05 17:31:09 +0530

    light sensor

--- app/src/main/java/com/example/kotlindevelopment/MainActivity.kt
+++ app/src/main/java/com/example/kotlindevelopment/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.example.kotlindevelopment
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    var sensor:Sensor?=null
+    var sensorManager:SensorManager?=null
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+    }
+
+
+    var isRunning=false
+    override fun onSensorChanged(event: SensorEvent?) {
+
+        if(event!!.values[0]>40 && isRunning==false){
+            isRunning=true
+            try{
+                var mp= MediaPlayer()
+                mp.setDataSource("https://www.youtube.com/watch?v=sK7riqg2mr4")
+                mp.prepare()
+                mp.start()
+
+            }catch (ex:Exception){}
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sensorManager=getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor= sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager!!.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager!!.unregisterListener(this)
+    }
+
 }
