38e58c81f80446c17d8b7a91d64c26b6a18069f5
sameer2506 <61904042+sameer2506@users.noreply.github.com>
2021-07-05 17:01:31 +0530

    music player app

---
+++ app/src/main/java/com/example/kotlindevelopment/SongInfo.kt
@@ -0,0 +1,18 @@
+package com.example.kotlindevelopment
+
+/**
+ * Created by hussienalrubaye on 5/28/17.
+ */
+
+class SongInfo{
+
+    var Title:String?=null
+    var AuthorName:String?=null
+    var SongURL:String?=null
+    constructor(Title:String,AuthorName:String,SongURL:String){
+        this.Title=Title
+        this.AuthorName=AuthorName
+        this.SongURL=SongURL
+    }
+
+}
